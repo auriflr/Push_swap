ecf49194c7dedaf25a458e268c72e1298228bdf8
Aurelio Floris <afloris@e3r2p6.42roma.it>
2025-07-09 16:38:18 +0200

    little steps

---
+++ Makefile
@@ -0,0 +1,39 @@
+NAME = push_swap
+
+PRINTF_DIR = PRINTF_DIR
+LIBFT_DIR = Libft
+CC = gcc
+RM = rm -f
+CFLAGS = -Wall -Wextra -Werror
+PRINTF = $(PRINTF_DIR)/printf.a
+LIBFT = $(LIBFT_DIR)/libft.a
+
+SRCS =
+
+OBJS = $(SRCS:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJS) $(PRINTF) $(LIBFT)
+		$(CC) $(CFLAGS) -o $@ $^
+
+$(PRINTF):
+			make -C $(PRINTF_DIR)
+
+$(LIBFT):
+			make -C $(LIBFT_DIR)
+
+%.o: %.c include/printf.h include/push_swap.h include/libft.h
+		$(CC) $(CFLAGS) -I include -c $< -o $@
+	
+clean:
+		$(RM) $(OBJS)
+		make -C $(PRINTF_DIR) $(LIBFT_DIR) clean
+
+fclean: clean
+		$(RM) $(NAME)
+		make -C $(PRINTF_DIR) $(LIBFT_DIR) clean
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ include/push_swap.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afloris <afloris@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 16:34:05 by afloris           #+#    #+#             */
+/*   Updated: 2025/07/09 16:36:49 by afloris          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+#define	PUSH_SWAP_H
+
+#include "../include/libft.h"
+#include "../include/prinft.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+#endif
