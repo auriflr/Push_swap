3090331c41ce3c4c96fa00b9434805d28b9c9224
Aurelio Floris <babyf@MAC.station>
2025-07-13 13:00:21 +0200

    all operations + library

--- include/push_swap.h
+++ include/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afloris <afloris@student.42.fr>            +#+  +:+       +#+        */
+/*   By: babyf <babyf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 16:34:05 by afloris           #+#    #+#             */
-/*   Updated: 2025/07/12 15:21:50 by afloris          ###   ########.fr       */
+/*   Updated: 2025/07/13 12:58:15 by babyf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,18 @@ typedef struct		s_stack
 }					t_stack;
 
 /*Operations and operation utils*/
-
+void	push_a(t_stack *a, t_stack *b);
+void	push_b(t_stack *a, t_stack *b);
+void	rotate_a(t_stack *a);
+void	rotate_b(t_stack *b);
+void	rotate_r(t_stack *a, t_stack *b);
+void	rrotate_a(t_stack *a);
+void 	rrotate_b(t_stack *b);
+void	rrotate_r(t_stack *a, t_stack *b);
+void	swap_a(t_stack *a);
+void	swap_b(t_stack *b);
+void	swap_r(t_stack *a, t_stack *b);
+int		pop(t_stack *stack);
+void	push(t_stack *stack, int value);
 
 #endif
