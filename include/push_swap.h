46cc46f801da2285b42401a98d1132f0097561ed
Aurelio Floris <babyf@MacBook-Air-di-Auri.local>
2025-08-26 13:27:32 +0200

    starting from scratch

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: babyf <babyf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 16:34:05 by afloris           #+#    #+#             */
-/*   Updated: 2025/08/25 15:50:04 by babyf            ###   ########.fr       */
+/*   Updated: 2025/08/25 19:47:48 by babyf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@
 #include <stdio.h>
 #include <limits.h>
 
-/*keeps track of size and top node
-list of different variables*/
 typedef struct		s_stack
 {
 	int				num;
@@ -34,5 +32,24 @@ typedef struct		s_stack
 void	sa(t_stack *a);
 void	sb(t_stack *b);
 void	ss(t_stack *a, t_stack *b);
+void	pa(t_stack *a, t_stack *b);
+void	pb(t_stack *a, t_stack *b);
+void	ra(t_stack *a);
+void	rb(t_stack *b);
+void	rr(t_stack *a, t_stack *b);
+// reverse rotate functions
+void	push(t_stack *stack, int value);
+int		pop(t_stack *stack);
+
+/*Indexing*/
+int		find_minindex(t_stack *a);
+
+/*Error handling in sorting and arguments*/
+int		check_valid(int ac, char **av);
+int		check_dup(int ac, char **av);
+
+/*Stack functions*/
+t_stack	*init_stack(void);
+void	sort_three(t_stack *a);
 
 #endif
